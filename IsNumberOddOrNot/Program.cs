006ecebbddd888fe9938127429bd8d5e76a7d289
LuisPulenta <luisalbertonu@gmail.com>
2024-03-16 18:36:00 -0300

    01-Número Par o Impar

---
+++ IsNumberOddOrNot/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("*** NUMERO PAR O IMPAR ***");
+Console.WriteLine("--------------------------");
+var numberString = string.Empty;
+do
+{
+    Console.Write("Ingrese Número ENTERO o 'S' para salir: ");
+    numberString = Console.ReadLine();
+
+    if (numberString!.ToUpper() == "S")
+    {
+        continue;
+    }
+
+    int numberInt;
+
+    if (int.TryParse(numberString, out numberInt ))
+    {
+        numberInt = int.Parse(numberString!);
+
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"{numberInt} es PAR");
+        }
+        else
+        {
+            Console.WriteLine($"{numberInt} es IMPAR");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"El valor ingresado {numberString} no es un número entero");
+    }
+}
+while (numberString.ToUpper() != "S");
+
+Console.WriteLine("--------------------------");
+Console.WriteLine("***         FIN        ***");
+Console.WriteLine("--------------------------");
