9835fba82f925c9ece29be9a65e56dd10dcf6ac0
LuisPulenta <luisalbertonu@gmail.com>
2024-03-17 14:46:27 -0300

    MultipleNumber

---
+++ MultipleNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using Shared;
+
+Console.WriteLine("*** NUMERO MULTIPLO ***");
+Console.WriteLine("-----------------------");
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    
+
+    if (a % b ==0)
+    {
+        Console.WriteLine($"{a} es múltiplo de {b}");
+    }
+
+    else if (b%a==0)
+    {
+        Console.WriteLine($"{b} es múltiplo de {a}");
+    }
+
+    else
+    {
+        Console.WriteLine($"{a} y {b} no son múltiplos ");
+    }
+    Console.WriteLine("------------------------");
+} while (true);
---
+++ OrderThreeNumbers/Program.cs
@@ -0,0 +1,57 @@
+﻿using Shared;
+
+Console.WriteLine("*** ORDENAR 3 NUMEROS ***");
+Console.WriteLine("-------------------------");
+
+do
+{
+    var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+    var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+    var c = ConsoleExtension.GetInt("Ingrese tercer número: ");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"El número mayor es: {a}");
+        if (b > c)
+        {
+            Console.WriteLine($"El número del medio es: {b}");
+            Console.WriteLine($"El número menor es: {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El número del medio es: {c}");
+            Console.WriteLine($"El número menor es: {b}");
+        }
+    }
+
+    else if (b > a && b > c)
+    {
+        Console.WriteLine($"El número mayor es: {b}");
+        if (a > c)
+        {
+            Console.WriteLine($"El número del medio es: {a}");
+            Console.WriteLine($"El número menor es: {c}");
+        }
+        else
+        {
+            Console.WriteLine($"El número del medio es: {c}");
+            Console.WriteLine($"El número menor es: {a}");
+        }
+    }
+
+    else
+    {
+        Console.WriteLine($"El número mayor es: {c}");
+        if (a > b)
+        {
+            Console.WriteLine($"El número del medio es: {a}");
+            Console.WriteLine($"El número menor es: {b}");
+        }
+        else
+        {
+            Console.WriteLine($"El número del medio es: {b}");
+            Console.WriteLine($"El número menor es: {a}");
+        }
+    }
+    Console.WriteLine("------------------------");
+} while (true);
