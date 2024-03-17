1527c9117ad2bab2d76f5a67b9cde51b0e022235
LuisPulenta <luisalbertonu@gmail.com>
2024-03-17 13:36:06 -0300

    TheGreatestNumber

---
+++ TheGreatestNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+
+Console.WriteLine("*** NUMERO MAYOR ***");
+Console.WriteLine("--------------------");
+
+do { 
+var a = ConsoleExtension.GetInt("Ingrese primer número: ");
+var b = ConsoleExtension.GetInt("Ingrese segundo número: ");
+var c = ConsoleExtension.GetInt("Ingrese tercer número: ");
+
+if(a>b && a > c)
+{
+    Console.WriteLine($"El número mayor es: {a}");
+}
+
+else if (b > a && b > c)
+{
+    Console.WriteLine($"El número mayor es: {b}");
+}
+
+else
+{
+    Console.WriteLine($"El número mayor es: {c}");
+}
+    Console.WriteLine("------------------------");
+} while(true);
